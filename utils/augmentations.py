d8752e205c494f2faa98f495b431259e847b13b6
Keerti Yadav <keertiy37@gmail.com>
2026-04-05 22:53:28 +0530

    Add files via upload

---
+++ utils/augmentations.py
@@ -0,0 +1,20 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform():
+    return A.Compose([
+        A.Resize(224, 224),
+        A.HorizontalFlip(p=0.5),
+        A.Rotate(limit=20, p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+        A.GaussianBlur(p=0.2),
+        A.Normalize(),
+        ToTensorV2()
+    ])
+
+def get_val_transform():
+    return A.Compose([
+        A.Resize(224, 224),
+        A.Normalize(),
+        ToTensorV2()
+    ])
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,1 +1,32 @@
+import os
+import cv2
+from torch.utils.data import Dataset
 
+class BrainTumorDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.image_paths = []
+        self.labels = []
+        self.transform = transform
+
+        classes = sorted(os.listdir(root_dir))
+
+        for label, cls in enumerate(classes):
+            cls_path = os.path.join(root_dir, cls)
+            for img in os.listdir(cls_path):
+                self.image_paths.append(os.path.join(cls_path, img))
+                self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.image_paths[idx])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        label = self.labels[idx]
+
+        if self.transform:
+            augmented = self.transform(image=img)
+            img = augmented["image"]
+
+        return img, label
